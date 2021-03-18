cad389adb138063d39a5121923db3528be599af6
VladimirDranitsa <79464479+VladimirDranitsa@users.noreply.github.com>
2021-03-18 22:24:08 +0300

    init: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,17 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform( arr ) {
+  const alteredArray = []
+  for (let i = 0; i < arr.length; i++) {
+    if ( arr[i - 1] === '--discard-next') {
+      continue
+    } else if ( arr[i + 1] === '--discard-prev'){
+      continue
+    } else if ( arr[i] === '--double-next'){
+      alteredArray.push(arr[i +1])
+    } else if ( arr[i +1] === '--double-prev') {
+      alteredArray.push(arr[i])
+    } else {
+      alteredArray.push(arr[i])
+    }
+  }
+  return alteredArray
+}
