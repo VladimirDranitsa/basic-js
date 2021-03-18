05e74f6ea3b7f4ac66cd02157c9f210902065b71
VladimirDranitsa <79464479+VladimirDranitsa@users.noreply.github.com>
2021-03-18 22:58:56 +0300

    init: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,14 +1,18 @@
 module.exports = function transform( arr ) {
   const alteredArray = []
   for (let i = 0; i < arr.length; i++) {
-    if ( arr[i - 1] === '--discard-next') {
+    if (arr[i - 1] === '--discard-next') {
       continue
-    } else if ( arr[i + 1] === '--discard-prev'){
+    } else if (arr[i + 1] === '--discard-prev') {
       continue
-    } else if ( arr[i] === '--double-next'){
-      alteredArray.push(arr[i +1])
-    } else if ( arr[i +1] === '--double-prev') {
+    } else if (arr[i] === '--double-next') {
+      alteredArray.push(arr[i + 1])
+    } else if (arr[i + 1] === '--double-prev') {
       alteredArray.push(arr[i])
+    } else if ( arr[i] === '--double-prev' || arr[i] === '--discard-prev' ||  arr[i] === '--discard-next' ){
+      continue
+    } else if ( arr[i] === ' ' || arr[i] === 'undefined'){
+      continue
     } else {
       alteredArray.push(arr[i])
     }
