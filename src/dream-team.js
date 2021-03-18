9a56271f88441a3bca90aa4ca1bbff28c88495c3
VladimirDranitsa <79464479+VladimirDranitsa@users.noreply.github.com>
2021-03-18 20:53:03 +0300

    init: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  const firstLetterOfName = []
+  if ( Array.isArray(members) === false){
+    return false
+  }
+  for (let i = 0; i < members.length; i++){
+    if (typeof members[i] === 'string' && members[i] !== ' '){
+      firstLetterOfName.push(members[i].trim().substring(0, 1).toUpperCase())
+    }
+  }
+  return firstLetterOfName.sort().join('')
+}
